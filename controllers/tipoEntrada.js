1f3202adf69991617ff03acaaec20f1817439459
ADEP-123 <andep210399@gmail.com>
2023-05-17 17:02:02 -0500

    fix(tipoEntrada): :bug: always disabled fixed
    fixed bug who keep alwasy disabled the inputs

--- controllers/tipoEntrada.js
+++ controllers/tipoEntrada.js
@@ -3,10 +3,13 @@ export function tipoEntrada(formu) {
   const latitud = document.getElementById("lat");
   const longitud = document.getElementById("long");
   const radioBusqueda = document.getElementById("rad");
-  console.log(datos);
   if (datos.tipEntr === "1") {
     latitud.disabled = true;
     longitud.disabled = true;
     radioBusqueda.disabled = true;
+  } else {
+    latitud.disabled = false;
+    longitud.disabled = false;
+    radioBusqueda.disabled = false;
   }
 }
